bd0251509c942cc635f1601146d1770cfb3ac4a5
Mjj4682 <jjmoon4682@gmail.com>
2022-10-26 15:39:26 +0900

    feat: initial setup complete

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const routes = require("./src/routes");
 
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const morgan = require('morgan');
-const routes = require('./api/routes');
-const { globalErrorHandler } = require('./api/utils/error');
+const createApp = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    morgan(":method :url :status :res[content-length] - :response-time ms")
+  );
+  app.use(routes);
+  return app;
+};
 
-app.use(cors());
-app.use(morgan('combined'));
-app.use(express.json());
-app.use(routes);
-app.use(globalErrorHandler);
-
-const port = process.env.PORT;
-
-app.listen(port, () => {
-    console.log(`Listening to request on 127.0.0.1:${port}`)
-});
+module.exports = { createApp };
