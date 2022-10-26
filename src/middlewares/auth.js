64ed506411219a339fd6210130107ad4c5487fbe
walter811 <walter811@naver.com>
2022-10-26 19:52:27 +0900

    feat: sign up & sign in 구현중

---
+++ src/middlewares/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const {asyncWrap} = require('./error');
+const { userService } = require('../services');
+
+const loginRequired = asyncWrap(async (req, res, next) => {
+    let accessToken = req.headers.authorization;
+
+    if (!accessToken) {
+        const error = new Error('NEED_ACCESSTOKEN');
+        error.statusCode = 401;
+
+        throw error;
+    }
+
+    const veryfiedToken = jwt.verify(accessToken, process.env.JWT_SECRET);
+    const user = await userService.getUserById(veryfiedToken.id);
+
+    if (!user) {
+        const error = new Error('INVALID_USER');
+        error.statusCode = 400;
+
+        throw error;
+    }
+
+    req.user = user;
+    next();
+});
+
+module.exports = {
+    loginRequired,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const userRouter = require('./userRouter');
+
 const router = express.Router();
 const statisticsRouter = require("./statisticsRouter");
 const boardRouter = require("./boardRouter");
@@ -6,5 +8,7 @@ const boardRouter = require("./boardRouter");
 router.use("/statistics", statisticsRouter.router);
 
 router.use("/board", boardRouter.router);
+
+router.use('/user', userRouter);
 
 module.exports = router;
